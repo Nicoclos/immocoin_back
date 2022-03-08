374c60e87b7efb3b8d5667f3d3f129e0cadebf17
Nico <nicoclos.bia@hotmail.fr>
2022-03-08 19:13:24 +0100

    solving issue with name attributes

--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_143905) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.integer "age"
     t.boolean "is_owner", default: false
     t.datetime "created_at", null: false
