8d32bcd6635bd3264c5e6c6ee205d1a962f9f0b2
jordan-creyelman <creyelmanjo@gmail.com>
2022-03-08 15:50:09 +0100

    beug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
+    puts "test"
     build_resource(sign_up_params)
     resource.save
     sign_up(resource_name, resource) if resource.persisted?
--- db/schema.rb
+++ db/schema.rb
@@ -37,14 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_152653) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.integer "age"
-    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["first_name"], name: "index_users_on_first_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
