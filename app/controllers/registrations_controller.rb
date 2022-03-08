1716c28756fbe8197048e4c407fd528b100cd892
jordan-creyelman <creyelmanjo@gmail.com>
2022-03-08 18:28:17 +0100

    error user jwt

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    puts "test"
     build_resource(sign_up_params)
     resource.save
     sign_up(resource_name, resource) if resource.persisted?
 
     render_jsonapi_response(resource)
-   
-   
   end
 end
